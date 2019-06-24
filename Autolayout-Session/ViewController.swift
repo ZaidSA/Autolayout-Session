3fd28af62180331b015da6fea0628803e9e44116
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-06-24 16:07:06 +0530

    First session code

---
+++ Autolayout-Session/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout-Session
+//
+//  Created by Zaid Pathan on 24/06/19.
+//  Copyright © 2019 Solution Analysts Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
